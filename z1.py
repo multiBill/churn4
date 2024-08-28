fa1ec799171f4652ab3015d881dd622d4abb88d7
multiBill <mohammadjalaldavooodian@gmail.com>
2024-08-29 00:08:09 +0330

    Update z1.py

--- z1.py
+++ z1.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import joblib
+#import joblib
 #from churnback2 import columns
 from sklearn.preprocessing import MinMaxScaler
 #from churnback2 import project
